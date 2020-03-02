f6dc31ec7e2980a5bf7298d346db159a0e7b794f
Roshan Nanu <rnanu@brandeis.edu>
2020-03-02 17:16:57 -0500

    Remove need for tempfile when using blech_waveforms_datashader

--- blechpy/plotting/blech_waveforms_datashader.py
+++ blechpy/plotting/blech_waveforms_datashader.py
@@ -16,11 +16,7 @@ from imageio import imread
 import shutil
 
 # A function that accepts a numpy array of waveforms and creates a datashader image from them
-def waveforms_datashader(waveforms, dir_name = "datashader_temp"):
-    tmp_fn = os.path.join(dir_name, 'tempfile_%i.png' % waveforms.shape[0])
-    if not os.path.isdir(dir_name):
-        os.makedirs(dir_name)
-
+def waveforms_datashader(waveforms):
     if waveforms.shape[0]==0:
         return None
     # Make a pandas dataframe with two columns, x and y, holding all the data. The individual waveforms are separated by a row of NaNs
@@ -39,27 +35,22 @@ def waveforms_datashader(waveforms, dir_name = "datashader_temp"):
     x[:-1] = x_values
 
     # Now make the dataframe
-    df = pd.DataFrame({'x': np.tile(x, new_waveforms.shape[0]), 'y': new_waveforms.flatten()})    
-
-    # Datashader function for exporting the temporary image with the waveforms
-    export = partial(export_image, background = "white", export_path=dir_name)
+    df = pd.DataFrame({'x': np.tile(x, new_waveforms.shape[0]), 'y': new_waveforms.flatten()})
 
     # Produce a datashader canvas
-    canvas = ds.Canvas(x_range = (np.min(x_values), np.max(x_values)), 
-               y_range = (df['y'].min() - 10, df['y'].max() + 10),
-               plot_height=1200, plot_width=1600)
+    canvas = ds.Canvas(x_range = (np.min(x_values), np.max(x_values)),
+                       y_range = (df['y'].min() - 10, df['y'].max() + 10),
+                       plot_height=1200, plot_width=1600)
     # Aggregate the data
-    agg = canvas.line(df, 'x', 'y', ds.count())   
+    agg = canvas.line(df, 'x', 'y', ds.count())
     # Transfer the aggregated data to image using log transform and export the temporary image file
-    export(tf.shade(agg, how='eq_hist'),'tempfile_%i' % waveforms.shape[0])
+    img = tf.shade(agg, how='eq_hist')
+    img = tf.set_background(img, 'white')
 
-    # Read in the temporary image file
-    img = imread(tmp_fn)
-    
     # Figure sizes chosen so that the resolution is 100 dpi
     fig,ax = plt.subplots(1, 1, figsize = (12,8), dpi = 200)
     # Start plotting
-    ax.imshow(img)
+    ax.imshow(img.to_pil())
     # Set ticks/labels - 10 on each axis
     ax.set_xticks(np.linspace(0, 1600, 10))
     ax.set_xticklabels(np.floor(np.linspace(np.min(x_values), np.max(x_values), 10)))
@@ -68,9 +59,6 @@ def waveforms_datashader(waveforms, dir_name = "datashader_temp"):
 
     # Delete the dataframe
     del df, waveforms, new_waveforms
-
-    # Also remove the directory with the temporary image files
-    os.remove(tmp_fn)
 
     # Return and figure and axis for adding axis labels, title and saving the file
     return fig, ax
