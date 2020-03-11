09995c93a6afeaf868a571e5bf97108623ec3daf
Roshan Nanu <rnanu@brandeis.edu>
2020-03-11 10:19:16 -0400

    update git ignore

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if os.path.isfile(requirementPath):
 
 setuptools.setup(
     name='blechpy',
-    version='2.0.17',
+    version='2.0.18',
     author='Roshan Nanu',
     author_email='roshan.nanu@gmail.com',
     description='Package for exrtacting, processing and analyzing Intan and OpenEphys data',
